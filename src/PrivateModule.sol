61a3a1801377e4069ffa1dedf1392d16373aef7f
emmaguo13 <emma@proofofalpha.io>
2022-10-24 12:38:05 -0700

    added semaphore, updated contracts not deployed

--- src/PrivateModule.sol
+++ src/PrivateModule.sol
@@ -2,11 +2,18 @@ pragma solidity ^0.8.0;
 
 import "zodiac/core/Module.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@semaphore-protocol/contracts/interfaces/ISemaphore.sol";
 import "./MerkleTreeWithHistory.sol";
 
 contract PrivateModule is Module, ReentrancyGuard {
-// contract PrivateModule {
 
+  ISemaphore public semaphore;
+
+  uint256 groupId;
+  mapping(uint256 => bytes32) users;
+
+  event NewTxn(bytes32 data);
+  event NewUser(uint256 identityCommitment, bytes32 username);
   event ZkSigModuleSetup(
     address indexed initiator,
     address indexed owner,
@@ -17,10 +24,14 @@ contract PrivateModule is Module, ReentrancyGuard {
   /// @param _owner Address of the  owner
   /// @param _avatar Address of the avatar (e.g. a Safe) basically thing executing the functions (relayer)
   /// @param _target Address of the contract that will call exec function - PASS TRANSACTIONS TO
+  /// @param _semaphore Address of the semaphore contract
+  /// @param _groupId semaphore groupId
   constructor(
     address _owner,
     address _avatar,
-    address _target
+    address _target,
+    address _semaphore,
+    uint256 _groupId
   )  {
 
     bytes memory initParams = abi.encode(
@@ -28,6 +39,10 @@ contract PrivateModule is Module, ReentrancyGuard {
       _avatar,
       _target
     );
+
+    semaphore = ISemaphore(semaphoreAddress);
+    groupId = _groupId;
+    semaphore.createGroup(groupId, 20, 0, address(this));
 
     setUp(initParams);
   }
@@ -53,20 +68,36 @@ contract PrivateModule is Module, ReentrancyGuard {
     emit ZkSigModuleSetup(msg.sender, _owner, _avatar, _target);
   }
 
+  function joinAsSigner(uint256 identityCommitment, bytes32 username) external {
+        semaphore.addMember(groupId, identityCommitment);
+
+        users[identityCommitment] = username;
+
+        emit NewUser(identityCommitment, username);
+  }
+
   /// @dev Executes a transaction initated by the AMB
   /// @param to Target of the transaction that should be executed
   /// @param value Wei value of the transaction that should be executed
   /// @param data Data of the transaction that should be executed
   /// @param operation Operation (Call or Delegatecall) of the transaction that should be executed
-  
-  // this executeTransaction function can take in any input, so we will be doing our checks here
+  /// @param merkleTreeRoot 
+  /// @param nullifierHash
+  /// @param proof We pass in the proof, verify it, and then execute 
+  // TODO: first test: just one user, one proof
   function executeTransaction(
     address to, // this is the target address, eg if you want the txn to push a button, this is the button
     // for us, don't we want the target to be anything?
     uint256 value,
     bytes memory data,
-    Enum.Operation operation
+    Enum.Operation operation,
+    uint256 merkleTreeRoot,
+    uint256 nullifierHash,
+    uint256[8] calldata proof
   ) public {
+    semaphore.verifyProof(groupId, merkleTreeRoot, greeting, nullifierHash, groupId, proof);
+
     require(exec(to, value, data, operation), "Module transaction failed");
+    emit NewTxn(data);
   }
 }
